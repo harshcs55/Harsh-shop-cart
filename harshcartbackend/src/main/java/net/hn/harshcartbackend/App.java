d6d7245e3010b28273fac7cf20334d741875eebc
Harsh Nair <harsh.nair.1@hotmail.com>
2019-05-19 16:38:07 +0530

    Project setup completed successfully

---
+++ harshcartbackend/src/main/java/net/hn/harshcartbackend/App.java
@@ -0,0 +1,13 @@
+package net.hn.harshcartbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
